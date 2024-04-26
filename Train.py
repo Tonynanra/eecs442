94dbbca3270de425cbedc40a5429df797ec54cdc
Tony <44306764+Tonynanra@users.noreply.github.com>
2024-04-25 21:18:11 -0400

    transfer

--- Train.py
+++ Train.py
@@ -5,10 +5,18 @@ import torch.optim as optim
 import config
 from tqdm import tqdm
 
-def train_func(D_pix, D_live, G_pix, G_live, loader, G_optimizer, D_optimizer, G_scaler, D_scaler, numEpochs=20):
-  BCE_loss = nn.BCELoss().cuda()
-  L1_loss=nn.L1Loss().cuda()
-  MSE_loss = nn.MSELoss().cuda()
+LAMBDA_CYCLE = 10
+LAMBDA_IDENTITY = 0
+
+def train_func(cycleGAN, loader, G_optimizer, D_optimizer, G_scaler, D_scaler, numEpochs=20):
+  '''
+  Using LSGAN loss for both discriminators and generators, 
+  and L1 loss for cycle and identity loss.
+  '''
+  
+  device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  L1_loss=nn.L1Loss().to(device)
+  MSE_loss = nn.MSELoss().to(device)
 
   H_reals = 0
   H_fakes = 0
@@ -18,23 +26,23 @@ def train_func(D_pix, D_live, G_pix, G_live, loader, G_optimizer, D_optimizer, G
     print('Start training epoch %d' % (epoch + 1))
     
     for idx, (pixel, scenery) in enumerate(loop):
-        pixel = pixel.to(config.DEVICE)
-        scenery = scenery.to(config.DEVICE)
+        pixel = pixel.to(device)
+        scenery = scenery.to(device)
 
         # Train Discriminators S and P
         with torch.cuda.amp.autocast():
-            fake_scenery = G_live(pixel)
-            D_live_real = D_live(scenery)
-            D_live_fake = D_live(fake_scenery.detach())
+            fake_scenery = cycleGAN.G_live(pixel)
+            D_live_real = cycleGAN.D_live(scenery)
+            D_live_fake = cycleGAN.D_live(fake_scenery.detach())
             H_reals += D_live_real.mean().item()
             H_fakes += D_live_fake.mean().item()
             D_live_real_loss = MSE_loss(D_live_real, torch.ones_like(D_live_real))
             D_live_fake_loss = MSE_loss(D_live_fake, torch.zeros_like(D_live_fake))
             D_live_loss = D_live_real_loss + D_live_fake_loss
 
-            fake_pixel = G_pix(scenery)
-            D_pix_real = D_pix(pixel)
-            D_pix_fake = D_pix(fake_pixel.detach())
+            fake_pixel = cycleGAN.G_pix(scenery)
+            D_pix_real = cycleGAN.D_pix(pixel)
+            D_pix_fake = cycleGAN.D_pix(fake_pixel.detach())
             D_pix_real_loss = MSE_loss(D_pix_real, torch.ones_like(D_pix_real))
             D_pix_fake_loss = MSE_loss(D_pix_fake, torch.zeros_like(D_pix_fake))
             D_pix_loss = D_pix_real_loss + D_pix_fake_loss
@@ -50,20 +58,20 @@ def train_func(D_pix, D_live, G_pix, G_live, loader, G_optimizer, D_optimizer, G
         # Train Generators S and P
         with torch.cuda.amp.autocast():
             # adversarial loss for both generators
-            D_live_fake = D_live(fake_scenery)
-            D_pix_fake = D_pix(fake_pixel)
+            D_live_fake = cycleGAN.D_live(fake_scenery)
+            D_pix_fake = cycleGAN.D_pix(fake_pixel)
             loss_G_live = MSE_loss(D_live_fake, torch.ones_like(D_live_fake))
             loss_G_pix = MSE_loss(D_pix_fake, torch.ones_like(D_pix_fake))
 
             # cycle loss
-            cycle_pixel = G_pix(fake_scenery)
-            cycle_scenery = G_live(fake_pixel)
+            cycle_pixel = cycleGAN.G_pix(fake_scenery)
+            cycle_scenery = cycleGAN.G_live(fake_pixel)
             cycle_pixel_loss = L1_loss(pixel, cycle_pixel)
             cycle_scenery_loss = L1_loss(scenery, cycle_scenery)
 
             # identity loss (remove these for efficiency if you set lambda_identity=0)
-            identity_pixel = G_pix(pixel)
-            identity_scenery = G_live(scenery)
+            identity_pixel = cycleGAN.G_pix(pixel)
+            identity_scenery = cycleGAN.G_live(scenery)
             identity_pixel_loss = L1_loss(pixel, identity_pixel)
             identity_scenery_loss = L1_loss(scenery, identity_scenery)
 
@@ -83,6 +91,4 @@ def train_func(D_pix, D_live, G_pix, G_live, loader, G_optimizer, D_optimizer, G
         G_scaler.update()
 
 
-        loop.set_postfix(H_real=H_reals / (idx + 1), H_fake=H_fakes / (idx + 1))
-
-
+    loop.set_postfix(H_real=H_reals / (idx + 1), H_fake=H_fakes / (idx + 1))
---
+++ generator.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+	
+class generator(nn.Module):
+  # initializers
+	def __init__(self, scale : int=1):
+		super(generator, self).__init__()
+
+		# Unet generator encoder
+		self.conv1 = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1)
+		self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+		self.bn2 = nn.BatchNorm2d(128)
+		self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
+		self.bn3 = nn.BatchNorm2d(256)
+		self.conv4 = nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)
+		self.bn4 = nn.BatchNorm2d(512)
+		self.conv5 = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)
+		self.bn5 = nn.BatchNorm2d(512)
+		self.conv6 = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)
+		self.bn6 = nn.BatchNorm2d(512)
+		self.conv7 = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)
+		self.bn7 = nn.BatchNorm2d(512)
+		self.conv8 = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)
+
+		# Unet generator decoder
+
+		self.deconv1 = nn.ConvTranspose2d(512, 512, kernel_size=4, stride=2, padding=1)
+		self.dbn1 = nn.BatchNorm2d(512)
+		self.deconv2 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1)
+		self.dbn2 = nn.BatchNorm2d(512)
+		self.deconv3 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1)
+		self.dbn3 = nn.BatchNorm2d(512)
+		self.deconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1)
+		self.dbn4 = nn.BatchNorm2d(512)
+		self.deconv5 = nn.ConvTranspose2d(1024, 256, kernel_size=4, stride=2, padding=1)
+		self.dbn5 = nn.BatchNorm2d(256)
+		self.deconv6 = nn.ConvTranspose2d(512, 128, kernel_size=4, stride=2, padding=1)
+		self.dbn6 = nn.BatchNorm2d(128)
+		self.deconv7 = nn.ConvTranspose2d(256, 64, kernel_size=4, stride=2, padding=1)
+		self.dbn7 = nn.BatchNorm2d(64)
+		self.deconv8 = nn.ConvTranspose2d(128, 3, kernel_size=4, stride=2, padding=1)
+
+  # weight_init
+	def weight_init(self, mean, std):
+		for m in self._modules:
+			normal_init(self._modules[m], mean, std)
+
+	# forward method
+	def forward(self, input):
+
+		e1 = F.leaky_relu(self.conv1(input), negative_slope=0.2)
+		e2 = F.leaky_relu(self.bn2(self.conv2(e1)), negative_slope=0.2)
+		e3 = F.leaky_relu(self.bn3(self.conv3(e2)), negative_slope=0.2)
+		e4 = F.leaky_relu(self.bn4(self.conv4(e3)), negative_slope=0.2)
+		e5 = F.leaky_relu(self.bn5(self.conv5(e4)), negative_slope=0.2)
+		e6 = F.leaky_relu(self.bn6(self.conv6(e5)), negative_slope=0.2)
+		e7 = F.leaky_relu(self.bn7(self.conv7(e6)), negative_slope=0.2)
+		e8 = F.leaky_relu(self.conv8(e7), negative_slope=0.2)
+
+		# decoding
+
+		d1 = F.relu(self.dbn1(self.deconv1(e8)))
+		d1 = torch.cat([d1, e7], 1)
+		d2 = F.relu(self.dbn2(self.deconv2(d1)))
+		d2 = torch.cat([d2, e6], 1)
+		d3 = F.relu(self.dbn3(self.deconv3(d2)))
+		d3 = torch.cat([d3, e5], 1)
+		d4 = F.relu(self.dbn4(self.deconv4(d3)))
+		d4 = torch.cat([d4, e4], 1)
+		d5 = F.relu(self.dbn5(self.deconv5(d4)))
+		d5 = torch.cat([d5, e3], 1)
+		d6 = F.relu(self.dbn6(self.deconv6(d5)))
+		d6 = torch.cat([d6, e2], 1)
+		d7 = F.relu(self.dbn7(self.deconv7(d6)))
+		d7 = torch.cat([d7, e1], 1)
+		d8 = F.tanh(self.deconv8(d7))
+
+		output = d8
+
+		return output
