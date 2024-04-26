a2b9ac8dc294fc23ddc1c4b2e53935dcf2059e12
Tony <44306764+Tonynanra@users.noreply.github.com>
2024-04-25 23:37:42 -0400

    transform done

--- dataset.py
+++ dataset.py
@@ -6,16 +6,6 @@ from torch.utils.data import Dataset
 import numpy as np
 from random import shuffle
 
-transforms = A.Compose(
-    [
-        A.Resize(width=256, height=256),
-        A.HorizontalFlip(p=0.5),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
-        ToTensorV2(),
-    ],
-    additional_targets={"image0": "image"},
-)
-
 class PixelSceneryDataset(Dataset):
     '''
     Adopted from https://github.com/Hexanol777/PixelGAN/blob/main/dataset.py
@@ -25,14 +15,24 @@ class PixelSceneryDataset(Dataset):
         self.root_scenery = root_scenery
         self.transform = transform
 
-        self.pixel_images = shuffle(os.listdir(root_pixel))
-        self.scenery_images = shuffle(os.listdir(root_scenery))
+        self.pixel_images = os.listdir(root_pixel)
+        self.scenery_images = os.listdir(root_scenery)
         self.length_dataset = min(len(self.pixel_images), len(self.scenery_images)) # in case of datasets not having the same length
         self.pixel_len = len(self.pixel_images)
         self.scenery_len = len(self.scenery_images)
 
     def __len__(self):
         return self.length_dataset
+    
+    def transforms(self, image):
+        transforms = A.Compose([   
+            A.CenterCrop(min(image.shape[:2]), min(image.shape[:2])),
+            A.Resize(width=256, height=256),
+            A.HorizontalFlip(p=0.5),
+            A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
+            ToTensorV2(),
+            ])
+        return transforms(image)
 
     def __getitem__(self, index):
         pixel_img = self.pixel_images[index % self.pixel_len]
@@ -45,9 +45,8 @@ class PixelSceneryDataset(Dataset):
         scenery_img = np.array(Image.open(scenery_path).convert("RGB"))
 
         if self.transform:
-            augmentations = self.transform(image=pixel_img, image0=scenery_img)
-            pixel_img = augmentations["image"]
-            scenery_img = augmentations["image0"]
+            pixel_img = self.transforms(pixel_img)
+            scenery_img = self.transforms(scenery_img)
 
         return pixel_img, scenery_img
 
